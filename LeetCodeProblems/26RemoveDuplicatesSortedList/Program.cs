93cf2edf3d59a40f2153f6e2629c153ddb76b645
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-13 08:10:05 -0300

    :feat:

---
+++ LeetCodeProblems/26RemoveDuplicatesSortedList/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] nums = new[] {0, 1, 2, 3, 3, 3, 4, 5, 5, 6, 6, 6, 7};
+
+RemoveDuplicates(nums);
+
+static int RemoveDuplicates(int[] nums)
+{
+    int j = 1;
+
+    for (int i = 1; i < nums.Length; i++)
+    {
+        if (nums[i] != nums[i - 1])
+        {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+
+    for (int i = j; i < nums.Length; i++)
+    {
+        nums[i] = int.MinValue;
+    }
+    
+    foreach (var num in nums)
+    {
+        Console.Write($"{num}; ");
+    }
+    
+    Console.WriteLine();
+    
+    return 0;
+}

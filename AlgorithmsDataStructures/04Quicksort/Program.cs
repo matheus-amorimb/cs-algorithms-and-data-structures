8ed3f7455ad77110068fd4920d90347a33720df8
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-23 16:28:40 -0300

    :feat:

--- AlgorithmsDataStructures/04Quicksort/Program.cs
+++ AlgorithmsDataStructures/04Quicksort/Program.cs
@@ -10,8 +10,6 @@ List<int> sortedList = QuickSort(list);
  
  static List<int> QuickSort(List<int> list)
  {
-     List<int> sortedList = new List<int>();
-     
      if (list.Count < 2)
      {
          return list;
@@ -19,26 +17,31 @@ List<int> sortedList = QuickSort(list);
 
      List<int> partialLeft = new List<int>();
      List<int> partialRight = new List<int>();
-     
-     int pivot = list[0];
 
-     for (int i = 1; i < list.Count; i++)
+     var random = new Random();
+     var randomIndex = random.Next(list.Count - 1);
+     int pivot = list[randomIndex];
+
+     for (int i = 0; i < list.Count; i++)
      {
-         if (list[i] < pivot)
+         if (i != randomIndex)
          {
-             partialLeft.Add(list[i]);
-         }
-         else
-         {
-             partialRight.Add(list[i]);
+             if (list[i] < pivot)
+             {
+                 partialLeft.Add(list[i]);
+             }
+             else
+             {
+                 partialRight.Add(list[i]);
+             }
          }
      }
 
      partialLeft = QuickSort(partialLeft);
      partialRight = QuickSort(partialRight);
 
-     sortedList = partialLeft.Append(pivot).ToList();
-     sortedList = sortedList.Concat(partialRight).ToList();
+     partialLeft.Add(pivot);
+     partialLeft.AddRange(partialRight);
      
-     return sortedList;
+     return partialLeft;
  }
--- AlgorithmsDataStructures/04Quicksort/obj/Debug/net8.0/04Quicksort.AssemblyInfo.cs
+++ AlgorithmsDataStructures/04Quicksort/obj/Debug/net8.0/04Quicksort.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("04Quicksort")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f00f2e22aa99be1672be725d6bb562bc3758d466")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e41f8371439bb73daed3993f3a40aa1cbf6208a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("04Quicksort")]
 [assembly: System.Reflection.AssemblyTitleAttribute("04Quicksort")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

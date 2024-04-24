37b3bc1b4cccc3ba00ea06bc484878fec94bb941
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-24 10:22:03 -0300

    :feat:

---
+++ LeetCodeProblems/169MajorityElement/Program.cs
@@ -0,0 +1,34 @@
+﻿
+List<int> nums = new List<int>() { 3, 2, 3 };
+
+Console.WriteLine(MajorityElement(nums));
+
+static int MajorityElement(List<int> nums)
+{
+    Dictionary<int, int> dict = new Dictionary<int, int>();
+
+    int numsLength = nums.Count;
+
+    for (int i = 0; i < numsLength; i++)
+    {
+        if (dict.ContainsKey(nums[i]))
+        {
+            int currentCounter = dict[nums[i]];
+            currentCounter++;
+
+            if (currentCounter > numsLength/2)
+            {
+                return nums[i];
+            }
+            
+            dict[nums[i]] = currentCounter;
+        }
+        else
+        {
+            dict.Add(nums[i], 1);
+        }
+        
+    }
+
+    return nums[0];
+}
--- LeetCodeProblems/80RemoveDuplicatesSortedArrayII/Program.cs
+++ LeetCodeProblems/80RemoveDuplicatesSortedArrayII/Program.cs
@@ -1,33 +1,36 @@
 ﻿int[] nums = new[] {0,0,1,1,1,1,2,3,3};
 
-RemoveDuplicates(nums);
+RemoveDuplicatesInPlace(nums);
+RemoveDuplicatesWithDict(nums);
 
 
-static int RemoveDuplicates(int[] nums)
+static int RemoveDuplicatesInPlace(int[] nums)
 {
     Dictionary<int, int> dict = new Dictionary<int, int>();
     int count;
     int pointer = 0;
+    int currentValue = nums[0];
+    int counterCurrentValue = 0;
     
     for (int i = 0; i < nums.Length; i++)
     {
-        if (dict.ContainsKey(nums[i]))
+        if (currentValue == nums[i])
         {
-            dict.TryGetValue(nums[i], out count);
-            if (count == 2)
+            counterCurrentValue++;
+            if (counterCurrentValue > 2)
             {
                 //
             }
             else
             {
-                dict[nums[i]] = 2;
                 nums[pointer] = nums[i];
                 pointer++;
             }
         }
         else
         {
-            dict.Add(nums[i], 1);
+            currentValue = nums[i];
+            counterCurrentValue = 1;
             nums[pointer] = nums[i];
             pointer++;
         }
@@ -40,4 +43,42 @@ static int RemoveDuplicates(int[] nums)
     }
     
     return pointer;
+}
+
+static int RemoveDuplicatesWithDict(int[] nums)
+{
+     Dictionary<int, int> dict = new Dictionary<int, int>();
+     int count;
+     int pointer = 0;
+
+     for (int i = 0; i < nums.Length; i++)
+     {
+         if (dict.ContainsKey(nums[i]))
+         {
+             dict.TryGetValue(nums[i], out count);
+             if (count == 2)
+             {
+                 //
+             }
+             else
+             {
+                 dict[nums[i]] = 2;
+                 nums[pointer] = nums[i];
+                 pointer++;
+             }
+         }
+         else
+         {
+             dict.Add(nums[i], 1);
+             nums[pointer] = nums[i];
+             pointer++;
+         }
+     }
+     
+     foreach (var num in nums)
+     {
+         Console.Write($"{num}, ");
+     }
+     
+     return pointer;
 }

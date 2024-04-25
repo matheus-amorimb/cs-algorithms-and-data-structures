9bb8e3e32eeea17c3ef1891b2723d5874825d29c
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-25 19:37:06 -0300

    :sparkles:

--- LeetCodeProblems/121BestTimeBuySellStock/Program.cs
+++ LeetCodeProblems/121BestTimeBuySellStock/Program.cs
@@ -1,6 +1,35 @@
-﻿
+﻿int[] prices = new int[] {7,6,4,3,1};
+
+Console.WriteLine(MaxProfit(prices));
 
 static int MaxProfit(int[] prices)
 {
+    int lowestValue = int.MaxValue;
+    int highestValue = int.MaxValue;
+    int maxProfit = 0;
+    
+    foreach (var price in prices)
+    {
+        if (price < lowestValue)
+        {
+            lowestValue = price;
+            highestValue = price;
+        
+        }
+
+        if (price > highestValue)
+        {
+            highestValue = price;
+        }
+        
+        int currentProfit = highestValue - lowestValue;
+
+        if (currentProfit > maxProfit)
+        {
+            maxProfit = currentProfit;
+        }
+    }
+
+    return maxProfit > 0 ? maxProfit : 0;
     
 }

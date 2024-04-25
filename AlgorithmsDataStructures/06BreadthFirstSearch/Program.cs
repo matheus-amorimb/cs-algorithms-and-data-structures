8a73a280c12064c2e4fe13f04ca796e45d937252
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-25 20:59:59 -0300

    :sparkles:

---
+++ AlgorithmsDataStructures/06BreadthFirstSearch/Program.cs
@@ -0,0 +1,46 @@
+﻿Dictionary<string, string[]> graph = new Dictionary<string, string[]>();
+
+graph["matheus"] = ["Luan", "Lohana", "Beatriz", "Cris"];
+graph["Cris"] = ["Zirlanda"];
+graph["Beatriz"] = ["Ricardo"];
+graph["Luan"] = ["Cris", "Layana", "PeterSeller"];
+graph["Lohana"] = [];
+
+Console.WriteLine(BreadthFirstSearch(graph, "matheus"));
+
+static bool isSeller(string name)
+{
+    return name.Contains("seller", StringComparison.OrdinalIgnoreCase);
+}
+
+static string BreadthFirstSearch(Dictionary<string, string[]> graph, string firstPerson)
+{
+    Queue<string> people = new Queue<string>();
+
+    foreach (var person in graph[firstPerson])
+    {
+        people.Enqueue(person);
+    }
+
+    while (people.Count > 0)
+    {
+        string individual = people.Dequeue();
+        if (isSeller(individual))
+        {
+            return individual;
+        }
+        else
+        {
+            if (graph.ContainsKey(individual))
+            {
+                foreach (var person in graph[individual])
+                {
+                    people.Enqueue(person);
+                }   
+            }
+        }   
+    }
+    
+    return "There is no seller";
+}
+

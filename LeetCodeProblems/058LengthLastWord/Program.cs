6067f2260574129a8ec1e7190e59c98defcd7d45
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-28 19:25:45 -0300

    :sparkles:

---
+++ LeetCodeProblems/058LengthLastWord/Program.cs
@@ -0,0 +1,28 @@
+﻿
+string s = "a";
+
+static int LengthOfLastWord(string s)
+{
+    int positionLastChar = s.Length -1;
+    int lengthLastWord = 0;
+    char lastChar = s[positionLastChar];
+        
+    while (lastChar == ' ')
+    {
+        positionLastChar--;
+        lastChar = s[positionLastChar];
+    }
+
+    while (lastChar != ' ')
+    {
+        lengthLastWord++;
+        positionLastChar--;
+        if(positionLastChar == -1)
+        {
+            break; 
+        }
+        lastChar = s[positionLastChar];
+    }
+
+    return lengthLastWord;
+}

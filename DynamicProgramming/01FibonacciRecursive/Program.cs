9c099b07a97d11aa2520a1f4c2550be8dc7b51f9
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-29 20:04:30 -0300

    :sparkles:

---
+++ DynamicProgramming/01FibonacciRecursive/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+Console.WriteLine(FibonacciRecursive(7));
+
+static int FibonacciRecursive(int n)
+{
+    if (n < 3)
+    {
+        return 1;
+    }    
+    
+    return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
+}

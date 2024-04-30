c22e60d8fb3d05371521e9d672d2be395e33da3f
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-30 19:36:49 -0300

    :sparkles:

---
+++ AlgorithmsDataStructures/13KnuthMorrisPratt/Program.cs
@@ -0,0 +1,34 @@
+﻿
+string haystack = "ab";
+string needle = "abc";
+
+Console.WriteLine(KnuthMorrisPratt(haystack, needle));
+
+static int KnuthMorrisPratt(string haystack, string needle)
+{
+    int needleLength = needle.Length;
+    int haystackLength = haystack.Length;
+
+    int needleCharPosition = 0;
+    int startPosition = -1;
+    
+    for (int index = 0; index < haystackLength; index++)
+    {
+        if (needle[needleCharPosition] == haystack[index])
+        {
+            if (needleCharPosition == needleLength - 1)
+            {
+                startPosition = index - needleLength + 1;
+                break;
+            }
+            needleCharPosition++;
+        }
+        else if(needleCharPosition != 0)
+        {
+            index--;
+            needleCharPosition = 0;
+        }
+    }
+
+    return startPosition;
+}
